bf87434b23016856ac2620fa3aa102ea2593c582
Alejandro Salas <asalas.wd@outlook.com>
2022-10-23 21:20:33 -0400

    Implement Comparable interface

--- src/org/cadenhead/ecommerce/Item.java
+++ src/org/cadenhead/ecommerce/Item.java
@@ -1,5 +1,10 @@
 package org.cadenhead.ecommerce;
 
-public class Item {
+public class Item implements Comparable {
 
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
