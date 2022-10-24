48311ce3686d6c34026658e46ad0353545964d2a
Alejandro Salas <asalas.wd@outlook.com>
2022-10-23 21:21:24 -0400

    Instantiate class variables

--- src/org/cadenhead/ecommerce/Item.java
+++ src/org/cadenhead/ecommerce/Item.java
@@ -1,7 +1,11 @@
 package org.cadenhead.ecommerce;
 
 public class Item implements Comparable {
-
+    private String id;
+    private String name;
+    private double retail;
+    private int quantity;
+    private double price;
 
     @Override
     public int compareTo(Object o) {
