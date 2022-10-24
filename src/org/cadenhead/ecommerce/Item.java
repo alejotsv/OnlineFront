2a4ee074fb5a87f586435991feb9e86b029cf419
Alejandro Salas <asalas.wd@outlook.com>
2022-10-23 21:28:20 -0400

    Create constructor

--- src/org/cadenhead/ecommerce/Item.java
+++ src/org/cadenhead/ecommerce/Item.java
@@ -7,8 +7,27 @@ public class Item implements Comparable {
     private int quantity;
     private double price;
 
+    public Item(String id, String name, String retail, String quantity) {
+        this.id = id;
+        this.name = name;
+        this.retail = Double.parseDouble(retail);
+        this.quantity = Integer.parseInt(quantity);
+
+        if (this.quantity>400) {
+            this.price = this.retail * .5D;
+        } else if (this.quantity>200) {
+            this.price = this.retail * .6D;
+        } else {
+            this.price = this.retail * .7D;
+        }
+        this.price = Math.floor(this.price * 100 + .5)/100;
+    }
+
+
+
     @Override
     public int compareTo(Object o) {
         return 0;
     }
+
 }
