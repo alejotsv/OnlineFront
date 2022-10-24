d02418b7651fd8074e38c54bcfbe6e992c3d8def
Alejandro Salas <asalas.wd@outlook.com>
2022-10-23 21:31:14 -0400

    Complete compareTo implementation

--- src/org/cadenhead/ecommerce/Item.java
+++ src/org/cadenhead/ecommerce/Item.java
@@ -13,9 +13,9 @@ public class Item implements Comparable {
         this.retail = Double.parseDouble(retail);
         this.quantity = Integer.parseInt(quantity);
 
-        if (this.quantity>400) {
+        if (this.quantity > 400) {
             this.price = this.retail * .5D;
-        } else if (this.quantity>200) {
+        } else if (this.quantity > 200) {
             this.price = this.retail * .6D;
         } else {
             this.price = this.retail * .7D;
@@ -23,11 +23,17 @@ public class Item implements Comparable {
         this.price = Math.floor(this.price * 100 + .5)/100;
     }
 
-
-
     @Override
-    public int compareTo(Object o) {
+    public int compareTo(Object obj){
+        Item temp = (Item) obj;
+        if (this.price < temp.price) {
+            return 1;
+        } else if (this.price > temp.price){
+            return -1;
+        }
         return 0;
     }
+
+
 
 }
