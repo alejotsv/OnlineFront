6eac40cd7220976cb6e26ca1bdd64a90a3883b36
Alejandro Salas <asalas.wd@outlook.com>
2022-10-25 21:53:48 -0400

    Add items to the storefront object

--- src/org/cadenhead/ecommerce/GiftShop.java
+++ src/org/cadenhead/ecommerce/GiftShop.java
@@ -3,6 +3,10 @@ package org.cadenhead.ecommerce;
 public class GiftShop {
     public static void main(String[] args) {
         Storefront store = new Storefront();
+        store.addItem("C01", "Mug", "9.99", "150");
+        store.addItem("C02", "LG Mug", "12.99", "82");
+        store.addItem("C03", "Mousepad", "10.49", "800");
+        store.addItem("D01", "T-Shirt", "16.99", "90");
 
     }
 }
