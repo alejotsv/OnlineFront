1adbb73150dca6ec88474d276c70f2666d3937f4
Alejandro Salas <asalas.wd@outlook.com>
2022-10-25 22:03:05 -0400

    Complete call to methods to display ordered list

--- src/org/cadenhead/ecommerce/GiftShop.java
+++ src/org/cadenhead/ecommerce/GiftShop.java
@@ -7,6 +7,21 @@ public class GiftShop {
         store.addItem("C02", "LG Mug", "12.99", "82");
         store.addItem("C03", "Mousepad", "10.49", "800");
         store.addItem("D01", "T-Shirt", "16.99", "90");
+        store.sort();
+
+        int size = store.getSize();
+
+        for (int i=0; i<size; i++){
+            Item show = store.getItem(i);
+            System.out.println(
+                    "Item ID " + show.getId() + "\n" +
+                    "Name: " + show.getName() + "\n" +
+                    "Price: " + show.getPrice() + "\n" +
+                    "Retail price: " + show.getRetail() + "\n" +
+                    "Quantity: " + show.getQuantity() + "\n" +
+                    "----------------------------------------"
+            );
+        }
 
     }
 }
